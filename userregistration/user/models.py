7ef8f01110829d53078653a39323140efef9414d
dolly_surewaves <dolly@surewaves.com>
2019-01-30 12:54:00 +0530

    migrations added

--- userregistration/user/models.py
+++ userregistration/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+    user_name = models.CharField(max_length=45, null=False, unique=True)
+    email = models.EmailField(max_length=200, null=False, unique=True)
+    password = models.CharField(max_length=45, null=False)
+    first_name = models.CharField(max_length=20, null=False)
+    last_name = models.CharField(max_length=20)
